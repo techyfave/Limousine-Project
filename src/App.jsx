0eba0c58bffec95037e5e50ee7aec894e08d971b
Teez-Jenz <tioluwaniemmyjay@gmail.com>
2025-03-22 14:04:45 +0000

    adding some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-// import Corporate from "./components/corporate/corporate";
-// import Corporate2 from "./components/corporate/Corporate2";
 import About from "./components/about/About";
-import Event from "./components/eventss/Event";
-import Footer from "./components/footer/Footer";
-import Nextstep from "./components/corporate/Nextstep";
-import Event2 from "./components/eventss/Event2";
-import Wedding from "./components/wedding/Wedding";
-import Wedding2 from "./components/wedding/Wedding2";
-import Legal from "./components/legal/Legal";
-import Legal2 from "./components/legal/Legal2";
+import Footer from "./components/Footer/Footer";
+import Privacy from "./components/privacy/Privacy";
 
 function App() {
   return (
     <>
       <div className="App">
         <Navbar />
-        <Legal />
-        <Legal2 />
-        <Nextstep />
+        <Privacy />
+
         <Footer />
       </div>
     </>
---
+++ src/components/privacy/Privacy.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Privacy() {
+  return (
+    <div>
+      <header className="about-header w-100 d-flex justify-content-center align-items-center custom-color text-white">
+        <h1 className="fs-2">Legal Transportation Services</h1>
+      </header>
+
+      <div className="m-5">
+        <h2 className="fs-3 fw-bold">Privacy and Policy</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Privacy;
