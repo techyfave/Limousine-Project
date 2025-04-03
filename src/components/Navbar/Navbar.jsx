1c3390c1d6a0a7727740d560495b38d9ad76e090
Mfon-obong Umoh <mfonobongumoh75@gmail.com>
2025-04-03 16:04:48 +0100

    nav bar adjustment

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,81 @@
+// import React, { useState } from "react";
+// import "./navbar.css";
+// import { Link } from "react-router-dom";
+
+// function Navbar() {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggleNavbar = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <header className="fixed-top">
+//       <nav className="navbar navbar-expand-lg bg-primary">
+//         <div className="container-fluid">
+//           <Link className="navbar-brand text-white" to="/">
+//             <img
+//               src="/public/img/tranparent.png"
+//               alt=""
+//               height={80}
+//               className="navbar-brand"
+//             />
+//           </Link>
+//           <button
+//             className="navbar-toggler"
+//             // type="button"
+//             // data-bs-toggle="collapse"
+//             // data-bs-target="#navbarNav"
+//             // aria-controls="navbarNav"
+//             // aria-expanded="false"
+//             // aria-label="Toggle navigation"
+//           >
+//             <span class="navbar-toggler-icon" onClick={toggleNavbar}></span>
+//           </button>
+
+//           <div
+//             className="collapse navbar-collapse justify-content-end fs-4"
+//             id="navbarNav"
+//           >
+//             <ul className="navbar-nav fs-5 ${isOpen ?'active :''}">
+//               <li className="nav-item">
+//                 <Link to="/about" className="nav-link text-white">
+//                   ABOUT
+//                 </Link>
+//               </li>
+//               <li className="nav-item">
+//                 <Link to="/services" className="nav-link text-white">
+//                   SERVICES
+//                 </Link>
+//               </li>
+//               <li class="nav-item">
+//                 <Link to="/contact" className="nav-link text-white">
+//                   CONTACT
+//                 </Link>
+//               </li>
+//               <li class="nav-item">
+//                 <Link to="/reservation" className="nav-link text-white">
+//                   RESERVATION
+//                 </Link>
+//               </li>
+//               <li class="nav-item">
+//                 <p className="nav-link text-white ">
+//                   LIVE AGENTS AVAILABLE 24/7
+//                 </p>
+//               </li>
+//               <li className="nav-item">
+//                 <Link className="nav-link text-white">+1 610-938-1411</Link>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
+//     </header>
+//   );
+// }
+
+// export default Navbar;
+
 import React, { useState } from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
@@ -16,28 +94,29 @@ function Navbar() {
           <Link className="navbar-brand text-white" to="/">
             <img
               src="/public/img/tranparent.png"
-              alt=""
+              alt="executive logo"
               height={80}
               className="navbar-brand"
             />
           </Link>
           <button
             className="navbar-toggler"
-            // type="button"
-            // data-bs-toggle="collapse"
-            // data-bs-target="#navbarNav"
-            // aria-controls="navbarNav"
-            // aria-expanded="false"
-            // aria-label="Toggle navigation"
+            type="button"
+            onClick={toggleNavbar}
+            aria-controls="navbarNav"
+            aria-expanded={isOpen}
+            aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon" onClick={toggleNavbar}></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
 
           <div
-            className="collapse navbar-collapse justify-content-end fs-4"
+            className={`navbar-collapse bg-primary justify-content-end fs-4 ${
+              isOpen ? 'show' : 'collapse'
+            }`}
             id="navbarNav"
           >
-            <ul className="navbar-nav fs-5 ${isOpen ?'active :''}">
+            <ul className="navbar-nav fs-5">
               <li className="nav-item">
                 <Link to="/about" className="nav-link text-white">
                   ABOUT
@@ -48,18 +127,18 @@ function Navbar() {
                   SERVICES
                 </Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link to="/contact" className="nav-link text-white">
                   CONTACT
                 </Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link to="/reservation" className="nav-link text-white">
                   RESERVATION
                 </Link>
               </li>
-              <li class="nav-item">
-                <p className="nav-link text-white ">
+              <li className="nav-item">
+                <p className="nav-link text-white">
                   LIVE AGENTS AVAILABLE 24/7
                 </p>
               </li>
