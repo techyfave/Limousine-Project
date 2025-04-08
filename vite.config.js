65f2c97a6844caf99bb72a8908ad486b8d5e6c75
techyfave <favourronenu@gmail.com>
2025-04-08 11:56:13 +0100

    uninstalling the vite static copy

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '.' // root of dist
-        }
-      ]
-    })
-  ]
+  plugins: [react()],
 })
